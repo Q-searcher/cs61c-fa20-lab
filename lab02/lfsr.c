6ff20d340edef7b3fd2288ec12db92085c22a354
Jiaqi Huang <1404992421@qq.com>
2026-04-08 01:00:49 +0800

    lab02

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -3,8 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lfsr.h"
+#include "bit_ops.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    unsigned bit0 = 
+    unsigned bit0 = get_bit(*reg, 0);
+    unsigned bit2 = get_bit(*reg, 2);
+    unsigned bit3 = get_bit(*reg, 3);
+    unsigned bit5 = get_bit(*reg, 5);
+
+    unsigned bit15 = bit0 ^ bit2 ^ bit3 ^bit5;
+
+    *reg = (*reg >> 1) + (bit15 << 15);
 }
 
