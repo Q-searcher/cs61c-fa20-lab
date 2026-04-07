cb969ca44b04342cd4d4d7603f3f96a3560900e2
Jiaqi Huang <1404992421@qq.com>
2026-04-07 22:48:41 +0800

    test

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -9,19 +9,27 @@ unsigned get_bit(unsigned x,
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns 
     // 0 or 1)
-    return -1;
+    unsigned mask = 1 << n;       // 创建掩码，第 n 位为 1
+    return (x & mask) >> n;       // 提取第 n 位并右移到最低位，返回 0 或 1
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
 void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
-    // YOUR CODE HERE
+    unsigned mask = 1 << n;
+    unsigned clear_mask = ~mask;    // 除了第n位外，其余全部为1
+    // 将第n位，置为零
+    *x = clear_mask & *x;
+    unsigned set_number = v << n;
+    *x = *x + set_number;    
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
-    // YOUR CODE HERE
+    unsigned mask = 1 << n;
+    *x = mask ^ *x;
+    
 }
 
--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -5,6 +5,6 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    unsigned bit0 = 
 }
 
